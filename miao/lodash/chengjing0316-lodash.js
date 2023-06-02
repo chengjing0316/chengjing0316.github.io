1a168017fdf425e170478946bfab66b8de9600e8
chengjing <976900401@qq.com>
2023-06-02 22:19:47 +0800

    6.2.1

---
+++ miao/lodash/chengjing0316-lodash.js
@@ -0,0 +1,41 @@
+var chengjing0316 = {
+  chunk: function (ary, size) {
+    let result = []
+    for(i = 0; i < ary.length; i += size){
+      result.push(ary.slice(i, i + size))
+    }
+    return result
+  },
+  compact: function(ary){
+    let result = []
+    for(i = 0; i < ary.length; i++){
+      if(ary[i] !== 0 && ary[i] !== null && ary[i] !== undefined && ary[i] !== NaN && ary[i] !== "" && ary[i] !== false){
+        result.push(ary.at(i))
+      }
+    }
+    return result
+  },
+  concat: function(...args){
+    let result = []
+    for(i = 0; i < args.length; i++){
+      result = result.concat(args[i])
+    }
+    return result
+  },
+  difference: function(...args){
+    let nums1 = args[0], arr = []
+    for(let i = 0; i < nums1.length; i++){
+      for(let j = 1; j < args.length; j++){
+        if(args[j].indexOf(nums1[i]) == -1){
+          arr.push(nums1[i])
+        }
+      }
+    }
+    return arr
+  },
+  fill: function(ary, val, start = 0, end = ary.length - 1){
+    let result = ary.fill(ary,val,start,end)
+    return result
+  }
+
+}
